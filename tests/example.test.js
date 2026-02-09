564e246b1a3c2f04336408d78511033a29dbc9f9
Amrit kumar Mahto <amrit.mahto@adypu.edu.in>
2026-02-09 18:11:56 +0530

    test

--- tests/example.test.js
+++ tests/example.test.js
@@ -9,3 +9,27 @@ test("Check for invalid sub total", () => {
     "Invalid subtotal",
   );
 });
+
+test("no coupon case",()=>{
+  expect(calculateFinalAmount(100)).toBe(100)
+})
+
+test("SAVE10 coupon",()=>{
+  expect(calculateFinalAmount(1000,"SAVE10")).toBe(850)
+})
+test("FLAT50 boundary case",()=>{
+  expect(calculateFinalAmount(100,"FLAT50")).toBe(50)
+})
+test("invalid subtotal throws error",()=>{
+  expect(()=>calculateFinalAmount(-100,"FLAT50")).toThrow("Invalid subtotal");
+  expect(()=>calculateFinalAmount(NaN,"FLAT50")).toThrow("Invalid subtotal");
+  expect(()=>calculateFinalAmount(-100,"SAVE10")).toThrow("Invalid subtotal");
+  expect(()=>calculateFinalAmount(NaN,"SAVE10")).toThrow("Invalid subtotal");
+  
+})
+
+test("case-insensitive coupon",()=>{
+  expect(calculateFinalAmount(1000,"flat50")).toBe(900);
+  expect(calculateFinalAmount(1000,"save10")).toBe(850);
+})
+
